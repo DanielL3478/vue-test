5226095828005be01f7d3ea1e46c0090927225db
Daniel Llonch <llonchdaniel@gmail.com>
2024-04-11 00:05:23 -0400

    t
    t

---
+++ vue/src/views/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 :class="titleClass">Sign Up</h1>
+  <div>
+    <input type="text" placeholder="johndoe@gmail.com" />
+    <input type="password" placeholder="qwerty123" />
+    <button>Confirm</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const titleClass = ref("title")
+</script>
+
+<style>
+.title {
+  color: aquamarine;
+}
+</style>
